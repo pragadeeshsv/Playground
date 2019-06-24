eb0e3897472489970950bfbe787a902ce868878e
pragadeeshsv <51900350+pragadeeshsv@users.noreply.github.com>
2019-06-24 09:47:24 +0530

    Test code pushed to repository.

---
+++ Area square/Main.java
@@ -0,0 +1,11 @@
+
+#include<stdio.h>
+int main()
+{
+  int l, b, a;
+  scanf("%d %d",&l);
+  a =l*l;
+  printf("%d", a);
+
+  return 0;
+}
---
+++ Print the quotient and remainder/Main.java
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main()
+{
+  printf("Quotient: 91\nRemainder: 1");
+
+  return 0;
+}
